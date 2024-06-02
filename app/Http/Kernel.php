3f96c4f1d5d8b53421adb412b1d7e6b1bd8946b5
Histo <hihihisto@protonmail.com>
2024-06-02 10:24:27 +0800

    globaladmin middleware fix

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,7 +2,7 @@
 
 namespace App\Http;
 
-use App\Http\Middleware\isGlobalAdmin;
+use App\Http\Middleware\IsGlobalAdmin;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -67,6 +67,6 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'isGlobalAdmin' => \App\Http\Middleware\isGlobalAdmin::class,
+        'isGlobalAdmin' => IsGlobalAdmin::class,
     ];
 }
